ef6df941b3ebcbbb065b74aa90caee07f3d5d781
Philix27 <philixbob@gmail.com>
2025-03-08 14:03:24 +0100

    implement bank module resolvers

--- server/src/modules/bankAccount/bankAccount.module.ts
+++ server/src/modules/bankAccount/bankAccount.module.ts
@@ -4,10 +4,30 @@ import { BankAccountService } from "./bankAccount.service";
 import { NotificationModule } from "../notification/notification.module";
 import { NotificationService } from "../notification/notification.service";
 import { BankAccountResolver } from "./bankAccount.resolver";
+import { AuthModule } from "../auth/auth.module";
+import { JwtStrategy } from "../auth/jwt.strategy";
+import { GqlAuthGuard } from "../auth/gql.guard";
+import { JwtCryptoService } from "../auth/jwt.service";
+import { ConfigModule } from "@nestjs/config";
+import { JwtModule } from "@nestjs/jwt";
+import jwtConfig from "../auth/jwt.config";
 
 @Module({
-    imports: [CommonModule, NotificationModule],
-    providers: [BankAccountService, BankAccountResolver, NotificationService],
+    imports: [
+        CommonModule,
+        NotificationModule,
+        AuthModule,
+        JwtModule.registerAsync(jwtConfig.asProvider()),
+        ConfigModule.forFeature(jwtConfig),
+    ],
+    providers: [
+        BankAccountService,
+        BankAccountResolver,
+        NotificationService,
+        JwtStrategy,
+        GqlAuthGuard,
+        JwtCryptoService,
+    ],
     exports: [],
 })
 export class BankAccountModule {}
--- server/src/modules/bankAccount/bankAccount.resolver.ts
+++ server/src/modules/bankAccount/bankAccount.resolver.ts
@@ -1,63 +1,56 @@
-import { Args, Mutation, Resolver } from "@nestjs/graphql";
+import { Args, Context, Mutation, Query, Resolver } from "@nestjs/graphql";
 
 import { BankAccountService } from "./bankAccount.service";
 import {
-    BankAccount_Create,
-    Auth_Login,
-    Auth_Logout,
-    Auth_ResetPassword,
-    Auth_sendEmailOtp,
-    Auth_verifyEmailOtp,
+    BankAccount_CreateInput,
+    BankAccount_DeleteInput,
+    BankAccount_DeleteResponse,
+    BankAccount_Response,
 } from "./bankAccount.dto";
-import { UserDto } from "../user/user.dto";
+import { GqlAuthGuard } from "../auth/gql.guard";
+import { UseGuards } from "@nestjs/common";
 
-@Resolver((of: any) => UserDto)
+@Resolver((of: any) => BankAccount_Response)
 export class BankAccountResolver {
     constructor(private readonly service: BankAccountService) {}
 
-    @Mutation((returns) => String)
-    async bankAccount_createAccount(
-        @Args("input") input: BankAccount_Create
-    ): Promise<String> {
-        const res = await this.service.createAccount(input);
+    @Mutation((returns) => BankAccount_Response)
+    async bankAccount_create(
+        @Context() context: any,
+        @Args("input") input: BankAccount_CreateInput
+    ): Promise<BankAccount_Response> {
+        const user = context.req.user;
+        const res = await this.service.create({
+            userId: user.id,
+            ...input,
+        });
 
         return res!;
     }
 
-    @Mutation((returns) => String)
-    async bankAccount_login(@Args("input") input: Auth_Login) {
-        const res = await this.service.login(input);
-
-        return res!;
-    }
-
-    @Mutation((returns) => String)
-    async bankAccount_logout(@Args("input") input: Auth_Logout) {
-        const res = await this.service.logout(input);
-
-        return res!;
-    }
-
-    @Mutation((returns) => String)
-    async bankAccount_verifyEmailOtp(
-        @Args("input") input: Auth_verifyEmailOtp
+    @Mutation((returns) => BankAccount_DeleteResponse)
+    async bankAccount_delete(
+        @Context() context: any,
+        @Args("input") input: BankAccount_DeleteInput
     ) {
-        const res = await this.service.verifyEmailOtp(input);
+        const user = context.req.user;
+        const res = await this.service.delete({
+            userId: user.id,
+            ...input,
+        });
 
         return res!;
     }
 
-    @Mutation((returns) => String)
-    async bankAccount_sendEmailOtp(@Args("input") input: Auth_sendEmailOtp) {
-        const res = await this.service.sendEmailOtp(input);
+    @UseGuards(GqlAuthGuard)
+    @Query((returns) => BankAccount_Response)
+    async bankAccount_getAll(
+        @Context() context: any
+    ): Promise<BankAccount_Response[]> {
+        const { userId } = context.req;
+        console.log("In bank resolver: ", userId);
+        const res = await this.service.getAll({ userId });
 
-        return res!;
-    }
-
-    @Mutation((returns) => String)
-    async bankAccount_resetPassword(@Args("input") input: Auth_ResetPassword) {
-        const res = await this.service.resetPassword(input);
-
-        return res!;
+        return res;
     }
 }
