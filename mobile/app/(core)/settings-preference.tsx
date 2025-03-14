7bfbda0694de2f68044641dd14c4145f86a7f998
Philix27 <philixbob@gmail.com>
2025-03-14 19:39:03 +0100

    [mobile]: add settings related pages

---
+++ mobile/app/(core)/settings-preference.tsx
@@ -0,0 +1,36 @@
+import { Image, SafeAreaView, StyleSheet } from "react-native";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import { appColor } from "@/lib/color";
+import HeaderBar from "@/components/Header";
+
+export default function MinisafeScreen() {
+  return (
+    <SafeAreaView>
+      <HeaderBar title="Preference" />
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">Welcome!</ThemedText>
+      </ThemedView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+    backgroundColor: appColor().background,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: "absolute",
+  },
+});
