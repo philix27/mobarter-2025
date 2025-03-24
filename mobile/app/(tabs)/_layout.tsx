4f3010bf47384b5b8c55fce906987c4df3b0648c
Philix27 <philixbob@gmail.com>
2025-03-24 04:31:19 +0100

    mobile:  add dummy data to p2p advert

--- mobile/app/(tabs)/_layout.tsx
+++ mobile/app/(tabs)/_layout.tsx
@@ -2,11 +2,14 @@ import { Tabs } from "expo-router";
 import React from "react";
 import { AppTabBar } from "@/components/BottomTabBar";
 
+
 export default function TabLayout() {
   return (
-    <Tabs tabBar={(props) => <AppTabBar {...props} />}>
-      <Tabs.Screen name="home" />
-      <Tabs.Screen name="p2p" />
-    </Tabs>
+    <>
+      <Tabs tabBar={(props) => <AppTabBar {...props} />}>
+        <Tabs.Screen name="home" />
+        <Tabs.Screen name="p2p" />
+      </Tabs>
+    </>
   );
 }
