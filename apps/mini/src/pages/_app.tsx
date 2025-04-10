804a16b1728ce4aea6e20e559052931475b23e80
Philix27 <philixbob@gmail.com>
2025-04-10 06:31:01 +0100

    remove rainbowkit

--- apps/mini/src/pages/_app.tsx
+++ apps/mini/src/pages/_app.tsx
@@ -1,7 +1,4 @@
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
-import { connectorsForWallets } from '@rainbow-me/rainbowkit'
-// import '@rainbow-me/rainbowkit/styles.css'
-// import { injectedWallet } from '@rainbow-me/rainbowkit/wallets'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Analytics } from '@vercel/analytics/react'
 import type { AppProps } from 'next/app'
@@ -18,21 +15,8 @@ import { celo, celoAlfajores } from 'wagmi/chains'
 const API_KEY =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMsImVtYWlsIjp7ImlkIjozLCJhZGRyZXNzIjoiMHg0NjJFNUYyNzJCODQzMTU2MjgxMTEyNjc3OWRhNkVjYUU1MUE1QjQwIiwid2FsbGV0X2lkIjpudWxsLCJjaGFpblR5cGUiOiJFdGhlcmV1bSIsIm1pbmlwYXkiOnRydWUsImNyZWF0ZWRfYXQiOiIyMDI1LTA0LTA2VDE1OjMzOjIxLjg4M1oiLCJ1cGRhdGVkX2F0IjoiMjAyNS0wNC0wNlQxNTozMzoyMS44ODNaIiwidXNlcl9pZCI6M30sImlhdCI6MTc0NDAwNDM2NiwiZXhwIjoxNzQ0MDQ3NTY2fQ.nuT-l_rXFzKQDMYgzWDAvvnWNG2dhNpiPynwdbqlRvI'
 
-const connectors = connectorsForWallets(
-  [
-    {
-      groupName: 'Recommended',
-      wallets: [],
-    },
-  ],
-  {
-    appName: 'Celo Composer',
-    projectId: process.env.WC_PROJECT_ID ?? '044601f65212332475a09bc14ceb3c34',
-  }
-)
 
 const config = createConfig({
-  connectors,
   chains: [celo, celoAlfajores],
   transports: {
     [celo.id]: http(),
