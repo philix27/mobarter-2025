1a944bfff1734b6649d9e5ffa53f2bdaa90eb941
Philix27 <philixbob@gmail.com>
2025-04-14 09:20:23 +0100

    add base url

--- apps/mini/src/Root/chatwoot.tsx
+++ apps/mini/src/Root/chatwoot.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
 const windowA = window as any
+const BASE_URL = process.env.NEXT_PUBLIC_LIVE_CHAT_BASE_URL!
+const CHATWOOT_TOKEN = process.env.NEXT_PUBLIC_LIVE_CHAT!
 class ChatwootWidget extends React.Component {
   componentDidMount() {
     // Add Chatwoot Settings
@@ -13,7 +15,6 @@ class ChatwootWidget extends React.Component {
     }
     // Paste the script from inbox settings except the <script> tag(function (d, t) {
     function init(d: any, t: any) {
-      const BASE_URL = 'https://chatwoot-production-ea49.up.railway.app'
       const g = d.createElement(t),
         s = d.getElementsByTagName(t)[0]
       g.src = BASE_URL + '/packs/js/sdk.js'
@@ -22,7 +23,7 @@ class ChatwootWidget extends React.Component {
       s.parentNode.insertBefore(g, s)
       g.onload = function () {
         windowA.chatwootSDK.run({
-          websiteToken: 'ChCYDay9BNZhV46PMh3dSXhi',
+          websiteToken: CHATWOOT_TOKEN,
           baseUrl: BASE_URL,
         })
       }
--- apps/mini/src/Root/live-loader.tsx
+++ apps/mini/src/Root/live-loader.tsx
@@ -3,7 +3,12 @@ import { Chatwoot, LiveChatLoaderProvider } from 'react-live-chat-loader'
 
 export default function LiveLoader(props: { children: ReactNode }) {
   return (
-    <LiveChatLoaderProvider providerKey="ChCYDay9BNZhV46PMh3dSXhi" provider="chatwoot">
+    <LiveChatLoaderProvider
+      providerKey={process.env.NEXT_PUBLIC_LIVE_CHAT!}
+      provider="chatwoot"
+      baseUrl={process.env.NEXT_PUBLIC_LIVE_CHAT_BASE_URL}
+      //   baseUrl="support.mobarter.com"
+    >
       {props.children}
       <Chatwoot color="#ff6719" />
     </LiveChatLoaderProvider>
