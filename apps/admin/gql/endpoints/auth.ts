55dacb90ded8c07be86537fbd35f61321a7e004e
Philix27 <philixbob@gmail.com>
2025-05-01 12:47:36 +0100

    mini: update fx price based on country

---
+++ apps/admin/gql/endpoints/auth.ts
@@ -0,0 +1,40 @@
+import { useMutation } from "@apollo/client";
+import {
+  MutationAuth_LoginArgs,
+  Auth_ResetPasswordInput,
+  Auth_LoginDocument,
+  Mutation,
+  MutationAuth_SendEmailOtpArgs,
+  MutationAuth_VerifyEmailOtpArgs,
+  MutationAuth_CreateAccountArgs,
+} from "../__generated__/graphql";
+import { WrappedResponse, Input } from "./types";
+
+export const useAuthLogin = () =>
+  useMutation<WrappedResponse<"auth_login">, MutationAuth_LoginArgs>(
+    Auth_LoginDocument
+  );
+
+export const useAuth_resetPassword = () =>
+  useMutation<
+    { auth_resetPassword: Mutation["auth_resetPassword"] },
+    Input<Auth_ResetPasswordInput>
+  >(Auth_ResetPasswordDocument);
+
+export const useAuth_sendEmailOtp = () =>
+  useMutation<
+    WrappedResponse<"auth_sendEmailOtp">,
+    MutationAuth_SendEmailOtpArgs
+  >(Auth_SendEmailOtpDocument);
+
+export const useAuth_verifyEmailOtp = () =>
+  useMutation<
+    WrappedResponse<"auth_verifyEmailOtp">,
+    MutationAuth_VerifyEmailOtpArgs
+  >(Auth_VerifyEmailOtpDocument);
+
+export const useAuth_CreateAccount = () =>
+  useMutation<
+    WrappedResponse<"auth_createAccount">,
+    MutationAuth_CreateAccountArgs
+  >(Auth_CreateAccountDocument);
