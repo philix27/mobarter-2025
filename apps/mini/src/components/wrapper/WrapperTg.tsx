7476028e33428436002a3ba20d3ab910838f628d
Philix27 <philixbob@gmail.com>
2025-05-05 17:39:51 +0100

    move tg theme setup

--- apps/mini/src/components/wrapper/WrapperTg.tsx
+++ apps/mini/src/components/wrapper/WrapperTg.tsx
@@ -1,11 +1,7 @@
-import { backButton } from '@telegram-apps/sdk-react'
-import * as tg from '@telegram-apps/sdk-react'
-import { useRouter } from 'next/router'
-import { ReactNode, useEffect } from 'react'
+import { ReactNode } from 'react'
 
 import WrapperComp from './WrapperComps'
 import { useInitUserToken } from '@/src/Root/providers/TgContext'
-import { useThemeColor } from '@/src/styles/Color'
 
 export default function WrapperTg({
   hideBottomNav,
@@ -14,29 +10,6 @@ export default function WrapperTg({
   children: ReactNode
   hideBottomNav?: boolean
 }) {
-  const router = useRouter()
-
-  const theme = useThemeColor()
-  useEffect(() => {
-    tg.setMiniAppBackgroundColor(theme.bg as any)
-    tg.setMiniAppBottomBarColor(theme.bg as any)
-    tg.setMiniAppHeaderColor(theme.bg as any)
-  }, [theme.bg])
-
-  useEffect(() => {
-    if (hideBottomNav) {
-      backButton.show()
-    } else {
-      backButton.hide()
-    }
-  }, [hideBottomNav])
-
-  useEffect(() => {
-    backButton.onClick(() => {
-      router.back()
-    })
-  }, [router])
-
   useInitUserToken()
 
   return <WrapperComp hideBottomNav={hideBottomNav}>{props.children}</WrapperComp>
