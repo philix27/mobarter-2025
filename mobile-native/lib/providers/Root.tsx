7b414ad700026a1a9c699ed7dd11fbc6f6c869f3
philix27 <philixbob@gmail.com>
2025-09-20 21:04:08 +0100

    [rn-mobile]: add project providers

---
+++ mobile-native/lib/providers/Root.tsx
@@ -0,0 +1,46 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+import { ReactNode } from 'react'
+import { ThemeProvider } from './ThemeContext'
+import Toast from 'react-native-toast-message'
+import { AppStores } from '../zustand'
+import React from 'react'
+import ReactQueryProvider from './Tanstack'
+import { ThirdwebProvider } from 'thirdweb/react'
+import ResponseProvider from './ResponseProvider'
+import { env } from '../env'
+import TransactionsProvider from './TransactionsProvider'
+
+// const token = process.env.EXPO_PUBLIC_SERVER_TEST_TOKEN!
+
+export function RootProviders(props: { children: ReactNode }) {
+  const store = AppStores.useUserInfo()
+  const token = store.token
+
+  const apollo = new ApolloClient({
+    uri: env.BACKEND_GRAPHQL,
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    cache: new InMemoryCache(),
+  })
+
+  return (
+    <ReactQueryProvider>
+      <ThirdwebProvider>
+        <ApolloProvider client={apollo}>
+          {/* <WagmiProvider config={config}> */}
+          <ThemeProvider>
+            <ResponseProvider>
+              <TransactionsProvider>
+                {/* <Slot /> */}
+                {props.children}
+                <Toast position="top" />
+              </TransactionsProvider>
+            </ResponseProvider>
+          </ThemeProvider>
+          {/* </WagmiProvider> */}
+        </ApolloProvider>
+      </ThirdwebProvider>
+    </ReactQueryProvider>
+  )
+}
---
+++ mobile-native/lib/providers/index.ts
@@ -0,0 +1,3 @@
+export * from "./Root"
+export * from "./ThemeContext"
+export * from "./ResponseProvider"
