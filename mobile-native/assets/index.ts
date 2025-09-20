0733618fd8b3cf0517051d1c6f13ca3069108eb3
philix27 <philixbob@gmail.com>
2025-09-20 21:01:57 +0100

    Add app assets files

---
+++ mobile-native/assets/index.ts
@@ -0,0 +1,24 @@
+// export const AppAssets = {
+//   logo: {
+//     atom: require('@/assets/logo/atom.png'),
+//     bitcoin: require('@/assets/logo/bitcoin.png'),
+//     'celo-coin': require('@/assets/logo/celo-coin.png'),
+//     celo: require('@/assets/logo/celo.png'),
+//     cusd: require('@/assets/logo/cusd.png'),
+//     ether: require('@/assets/logo/ether.png'),
+//     glo: require('@/assets/logo/glo.png'),
+//     usdt: require('@/assets/logo/usdt.png'),
+//   },
+//   flags: {
+//     Cameroon: require('@/assets/flags/Cameroon.jpg'),
+//     Europe: require('@/assets/flags/Europe.png'),
+//     Ghana: require('@/assets/flags/Ghana.png'),
+//     Kenya: require('@/assets/flags/Kenya.png'),
+//     NGN: require('@/assets/flags/NGN.png'),
+//     SouthAfrica: require('@/assets/flags/SouthAfrica.png'),
+//     UK: require('@/assets/flags/UK.png'),
+//     US: require('@/assets/flags/US.png'),
+//   },
+// }
+
+export const successAnime = require('@assets/success.json')
