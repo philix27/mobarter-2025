e68264a290ab3fb5086688ce54d6091c496e806b
philix27 <philixbob@gmail.com>
2025-09-20 22:04:16 +0100

    [rn-mobile]: tabs section

---
+++ mobile-native/app/(tabs)/home.tsx
@@ -0,0 +1,9 @@
+import MarketScreen from '@/features/market'
+
+export default function Page() {
+  return (
+    <>
+      <MarketScreen />
+    </>
+  )
+}
---
+++ mobile-native/app/(tabs)/payments.tsx
@@ -0,0 +1,10 @@
+import { HeaderBar, Wrapper } from '@/components/layout'
+import ServicesScreen from '@/features/services'
+
+export default function Page() {
+  return (
+    <Wrapper>
+      <ServicesScreen />
+    </Wrapper>
+  )
+}
