a4bb1e0ef5ca79aadaa5fdf02cf2b2853b3fc6dd
philix27 <philixbob@gmail.com>
2025-09-20 23:10:56 +0100

    add form components ui

---
+++ mobile-native/components/forms/InputSelect.tsx
@@ -0,0 +1,64 @@
+import { Row, TView } from '@/components/ui'
+import React, { useState } from 'react'
+import { useColor } from '@/hooks/useColor'
+import { ScrollView, ViewStyle } from 'react-native'
+import { Label } from './Label'
+import { BtmSheet } from '@/components/layout'
+import ErrMsg from './ErrMsg'
+import { Ionicons } from '@expo/vector-icons'
+
+export function InputSelect(params: {
+  label?: string
+  value?: string
+  placeholder?: string
+  style?: ViewStyle
+  error?: string | undefined
+  items: { label: string; value: string; icon?: string; desc?: string }[]
+  onValueChange?: (itemValue: string) => void
+}) {
+  const theme = useColor()
+
+  const refRBSheet = BtmSheet.useRef()
+  const [selectedValue, setSelectedValue] = useState(params.value)
+
+  const getActive = () => {
+    const active = params.items.filter((val, i) => val.value === selectedValue)[0]
+    return active
+  }
+  return (
+    <>
+      <TView style={{ width: '100%' }}>
+        {params.label && <Label label={params.label} />}
+        <Row
+          title={!selectedValue ? 'Select' : getActive().label}
+          desc={getActive()?.desc}
+          imgUrl={getActive()?.icon}
+          trailing={<Ionicons name="caret-down" size={20} color={theme.muted} />}
+          onClick={() => {
+            refRBSheet.current!.open()
+          }}
+        />
+        {params.error && <ErrMsg msg={params.error} />}
+      </TView>
+
+      <BtmSheet.Modal ref={refRBSheet!}>
+        <ScrollView style={{width: '100%', paddingBottom: 20}}>
+          {params.items.map((item, i) => (
+            <Row
+              key={i}
+              title={item.label}
+              desc={item.value}
+              imgUrl={item.icon}
+              onClick={() => {
+                setSelectedValue(item.value)
+                if (params.onValueChange) {
+                  params.onValueChange(item.value)
+                }
+              }}
+            />
+          ))}
+        </ScrollView>
+      </BtmSheet.Modal>
+    </>
+  )
+}
---
+++ mobile-native/components/forms/InputText.tsx
@@ -0,0 +1,158 @@
+import {
+  TextInput,
+  StyleSheet,
+  KeyboardTypeOptions,
+  TouchableOpacity,
+  ViewStyle,
+  TextStyle,
+} from 'react-native'
+import { TView } from '../ui/TView'
+import { useColor } from '@/hooks/useColor'
+import { JSX, useState } from 'react'
+import { Feather } from '@expo/vector-icons'
+
+import ErrMsg from './ErrMsg'
+import { TText } from '../ui/TText'
+import { Label } from './Label'
+
+export function InputText(props: {
+  label?: string
+  leadingText?: string
+  desc?: string
+  style?: ViewStyle
+  inputStyle?: TextStyle
+  value?: string | undefined
+  disabled?: boolean | undefined
+  icon?: JSX.Element
+  trailingIcon?: JSX.Element
+  onTrailingIconPress?: VoidFunction
+  error?: string | undefined
+  onChangeText?: ((text: string) => void) | undefined
+  placeholder?: string | undefined
+  secureTextEntry?: boolean | undefined
+  keyboardType?: KeyboardTypeOptions | undefined
+}) {
+  const theme = useColor()
+  const [HidePassword, setHidePassword] = useState(props.secureTextEntry)
+  const [onFocus, setFocus] = useState(false)
+  return (
+    <>
+      <TView
+        style={[
+          styles.container,
+          {
+            // backgroundColor: theme.primary,
+            backgroundColor: theme.background,
+            position: 'relative',
+            // marginTop: 10,
+          },
+          props.style,
+        ]}
+      >
+        {props.label && <Label label={props.label} />}
+        <TView
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            backgroundColor: theme.card,
+            borderColor: onFocus ? theme.primary : theme.background,
+            borderWidth: 1,
+            borderRadius: 5,
+            width: '100%',
+          }}
+        >
+          {props.leadingText && (
+            <TView
+              style={{
+                backgroundColor: theme.card,
+                paddingLeft: 8,
+                paddingRight: 0,
+              }}
+            >
+              <TText style={{ fontSize: 16, color: theme.muted }}>{props.leadingText}</TText>
+            </TView>
+          )}
+
+          <TextInput
+            style={[
+              styles.input,
+              {
+                backgroundColor: theme.card,
+                color: theme.text,
+                paddingLeft: props.leadingText ? 4 : 10,
+                // width: '90%',
+              },
+              props.inputStyle,
+            ]}
+            keyboardType={props.keyboardType}
+            value={props.value}
+            onChangeText={props.onChangeText}
+            placeholder={props.placeholder}
+            secureTextEntry={HidePassword}
+            placeholderTextColor={theme.muted}
+            onFocus={() => setFocus(true)}
+            onBlur={() => setFocus(false)}
+            editable={props.disabled}
+          />
+          {props.secureTextEntry && (
+            <TView style={{ position: 'absolute', right: 10, top: 38 }}>
+              {HidePassword ? (
+                <Feather
+                  name="eye-off"
+                  size={20}
+                  color={theme.muted}
+                  onPress={() => setHidePassword(false)}
+                />
+              ) : (
+                <Feather
+                  name="eye"
+                  size={20}
+                  color={theme.muted}
+                  onPress={() => setHidePassword(true)}
+                />
+              )}
+            </TView>
+          )}
+
+          {props.trailingIcon && (
+            <TouchableOpacity
+              style={{
+                right: 10,
+                position: 'absolute',
+                // top: 40,
+                // backgroundColor: theme.primary,
+              }}
+              onPress={props.onTrailingIconPress}
+            >
+              {props.trailingIcon}
+            </TouchableOpacity>
+          )}
+        </TView>
+
+        {props.error && <ErrMsg msg={props.error} />}
+      </TView>
+    </>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    // marginTop: 10,
+    width: '100%',
+    // minWidth: "100%",
+    marginBottom: 0,
+  },
+  input: {
+    // height: 50,
+    paddingHorizontal: 10,
+    paddingVertical: 12,
+    fontSize: 16,
+    borderRadius: 5,
+    // width: '100%',
+    // minWidth: '90%',
+  },
+
+  icon: {
+    marginLeft: 10,
+  },
+})
